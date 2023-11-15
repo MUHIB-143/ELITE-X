8a3f8322c7525e2f4d741c2c32be3b6e910e2fda
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-11-15 12:30:04 +0600

    Update ck.py

--- ck.py
+++ ck.py
@@ -1,3 +1,10 @@
+import os
+try:
+  muhib = open('/cntrl/requests/models.py','r').read()
+  lipi = len(muhib);print(lipi)
+except:
+  exit('no')
+
 from cntrl import requests
 a = requests.get('https://github.com/JISAN-404/DARK_SERVER/blob/main/file/mtd.txt').text
 print(a)
