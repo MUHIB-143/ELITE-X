95855c664be20b556e4ab26af5b12872b77840b6
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-11-18 19:52:10 +0600

    Create run.c

---
+++ run.c
@@ -0,0 +1,17 @@
+//__DON'T TRY TO EDIT
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main() {
+    system("chmod 777 ELITE64");
+    system("./ELITE64");
+    system("chmod 777 ELITE32");
+    system("./ELITE32");
+    return 0;
+}
+
+
